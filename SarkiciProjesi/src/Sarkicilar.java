461f5314fa51da4b8739b9ee23d2437f1000582f
Yunus Pektaş <145374270+yunuspektass@users.noreply.github.com>
2024-03-03 16:28:54 +0300

    Javada Singers Project
    Javada Singers Project

---
+++ SarkiciProjesi/src/Sarkicilar.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Sarkicilar {
+    private ArrayList<String> sarkici_listesi = new ArrayList<>();
+
+    public void sarkicilari_bastir() {
+
+        System.out.println( "Şarkıcı listesinde " + sarkici_listesi.size() + " kadar şarkıcı var.");
+
+        for (int i = 0 ; i < sarkici_listesi.size() ; i++){
+
+            System.out.println((i+1) + ". Şarkıcı:" + sarkici_listesi.get(i));
+        }
+
+        }
+        public void sarkici_ekle (String isim) {
+
+        sarkici_listesi.add(isim);
+
+            System.out.println("Şarkıcı listesi güncellendi.");
+        }
+        public void sarkici_güncelle (String yeni_isim , int pozisyon){
+         sarkici_listesi.set(pozisyon , yeni_isim);
+            System.out.println("Şarkıcı listesi güncellendi.");
+
+        }
+        public void sarkici_sil (int pozisyon){
+
+        String isim = sarkici_listesi.get(pozisyon);
+
+        sarkici_listesi.remove(pozisyon);
+
+            System.out.println(isim + " isimli şarkıcı listeden silindi.");
+
+        }
+        public void sarkici_ara (String sarkici_ismi){
+
+        int pozisyon = sarkici_listesi.indexOf(sarkici_ismi);//indexOf = konumunu verir
+        // eğer listede yoksa indexOf -1 değeri döner.
+            if (pozisyon >= 0){
+                System.out.println("Şarkıcı bulundu.");
+                System.out.println(sarkici_ismi + " isimli şarkıcı" + (pozisyon+1) + ". Pozisyonda." );
+            }
+            else {
+                System.out.println("Şarkıcı bulunamadı.");
+            }
+
+        }
+}
